3d92c79d4be0c809c48aaa0c7422c9623b2263c5
kakao95490 <82544164+kakao95490@users.noreply.github.com>
2024-12-05 14:27:56 +0100

    models done

--- ToxixDetector.py
+++ ToxixDetector.py
@@ -1,6 +1,10 @@
+import ast
 import tkinter as tk
 import spacy
-import re  # Pour les expressions régulières
+import re  
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+import pandas as pd
 
 # Chargement du modèle de langue de spaCy
 nlp = spacy.load("en_core_web_md")
@@ -22,22 +26,49 @@ def preprocess_text(text):
     tokens = [
         token.lemma_.lower()
         for token in doc
-        if not token.is_stop and (not token.is_punct or token.text in {'!', '?'})
+        if not token.is_punct or token.text in {'!', '?'}
     ]
     
     
     return tokens
 
+def convert_with_dico(tokens, dico_df):
+    #convert tokens to numbers
+    #dico_df is a dataframe with 2 columns: 'word' and 'id'
+    tokens_id = []
+    for token in tokens:
+        try:
+            tokens_id.append(dico_df[dico_df['word'] == token]['id'].values[0])
+        except:
+            tokens_id.append(0)
+    return tokens_id
+
 def envoyer_texte():
     # Récupérer la valeur du champ de texte et la stocker dans une variable
-    texte_saisi = champ_texte.get("1.0", "end-1c")  # Récupérer tout le texte dans le widget Text
+    texte_saisi = champ_texte.get("1.0", "end-1c") 
     
     # Prétraiter le texte
     tokens = preprocess_text(texte_saisi)
     tokens = remplacer_mots_par_num(tokens)
+    tokens = convert_with_dico(tokens, dico_df)
+    #pading
+    tokens = pad_sequences([tokens], maxlen=200, padding='post')
     
-    # Afficher les tokens dans l'étiquette de l'interface
-    print(tokens)
+    # Prédire la toxicité du texte
+    prediction = model.predict(tokens)
+    
+    #6 output toxicity : toxic, severe_toxic, obscene, threat, insult, identity_hate
+    
+    # Afficher le résultat
+    etiquette_resultat.config(text=f"toxic: {prediction[0][0]:.2f}, severe_toxic: {prediction[0][1]:.2f}, obscene: {prediction[0][2]:.2f}, threat: {prediction[0][3]:.2f}, insult: {prediction[0][4]:.2f}, identity_hate: {prediction[0][5]:.2f}")
+    
+
+    
+    
+
+
+dico_df = pd.read_csv('./dataset/preprocessed/dico.csv')
+model = load_model('./models/categorisedToxicity.keras')
 
 # Création de la fenêtre principale
 fenetre = tk.Tk()
@@ -62,20 +93,17 @@ frame.pack(fill=tk.BOTH, padx=10, pady=10, expand=True)
 scrollbar = tk.Scrollbar(frame)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-# Création de la zone de texte (widget Text) qui occupe presque toute la fenêtre
 champ_texte = tk.Text(frame, wrap=tk.WORD, yscrollcommand=scrollbar.set)
 champ_texte.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
 
-# Lier la barre de défilement à la zone de texte
+
 scrollbar.config(command=champ_texte.yview)
 
-# Création d'un bouton pour envoyer le texte
 bouton_envoyer = tk.Button(fenetre, text="Envoyer", command=envoyer_texte)
 bouton_envoyer.pack(pady=10)
 
-# Création d'une étiquette pour afficher les résultats
 etiquette_resultat = tk.Label(fenetre, text="")
 etiquette_resultat.pack(pady=10)
 
-# Lancement de la boucle principale
+
 fenetre.mainloop()
